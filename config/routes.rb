3d70e4fd05f50e03889dc4c49ccd67e2c6496c64
Joshua Alphonse <joshalphonse@gmail.com>
2019-10-28 16:02:41 -0400

    Finished :)test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
       namespace :v1 do
-        resources :gorbyoyo, only: [:index, :create]
+        resources :gorbyoyo, only: [:index, :create, :destroy, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
